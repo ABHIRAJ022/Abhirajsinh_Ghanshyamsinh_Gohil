d0461bd4357e2bee36b73e6c5e8327f9795e4935
ABHIRAJ022 <rajput12557q@gmail.com>
2026-05-20 09:50:16 +0000

    UIUX

--- script.js
+++ script.js
@@ -5,19 +5,30 @@ function openProject(link) {
 function openImage(src) {
   let modal = document.getElementById("modal");
   let img = document.getElementById("modal-img");
+  let text = document.getElementById("modal-text");
 
   img.src = src;
+  text.innerText = "";
   modal.style.display = "block";
 }
 
-function showResult(text) {
+function showResult(textContent) {
   let modal = document.getElementById("modal");
-  let result = document.getElementById("modal-text");
+  let text = document.getElementById("modal-text");
 
-  result.innerText = text;
+  text.innerText = textContent;
+  document.getElementById("modal-img").src = "";
   modal.style.display = "block";
 }
 
 function closeModal() {
   document.getElementById("modal").style.display = "none";
 }
+
+// Close modal when clicking outside image
+window.onclick = function(event) {
+  let modal = document.getElementById("modal");
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
